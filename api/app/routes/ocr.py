8f1967253d2770779a5e7ee8da7e01790a002dd5
Bryan Hong <bryan@turbojets.net>
2025-05-02 12:56:35 -0700

    Adjustments to use Nvidia GPU and /ocr endpoint response as pickled hOCR

--- api/app/routes/ocr.py
+++ api/app/routes/ocr.py
@@ -4,11 +4,13 @@
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 
 
-from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status, Response
 
 from app.schemas import OCRBlock, OCRIn, OCRLine, OCROut, OCRPage, OCRWord
 from app.utils import get_documents, resolve_geometry
 from app.vision import init_predictor
+import pickle
+import torch
 
 router = APIRouter()
 
@@ -25,42 +27,55 @@ async def perform_ocr(request: OCRIn = Depends(), files: list[UploadFile] = [Fil
 
     out = predictor(content)
 
-    results = [
-        OCROut(
-            name=filenames[i],
-            orientation=page.orientation,
-            language=page.language,
-            dimensions=page.dimensions,
-            items=[
-                OCRPage(
-                    blocks=[
-                        OCRBlock(
-                            geometry=resolve_geometry(block.geometry),
-                            objectness_score=round(block.objectness_score, 2),
-                            lines=[
-                                OCRLine(
-                                    geometry=resolve_geometry(line.geometry),
-                                    objectness_score=round(line.objectness_score, 2),
-                                    words=[
-                                        OCRWord(
-                                            value=word.value,
-                                            geometry=resolve_geometry(word.geometry),
-                                            objectness_score=round(word.objectness_score, 2),
-                                            confidence=round(word.confidence, 2),
-                                            crop_orientation=word.crop_orientation,
-                                        )
-                                        for word in line.words
-                                    ],
-                                )
-                                for line in block.lines
-                            ],
-                        )
-                        for block in page.blocks
-                    ]
-                )
-            ],
-        )
-        for i, page in enumerate(out.pages)
-    ]
+#    results = [
+#        OCROut(
+#            name=filenames[i],
+#            orientation=page.orientation,
+#            language=page.language,
+#            dimensions=page.dimensions,
+#            items=[
+#                OCRPage(
+#                    blocks=[
+#                        OCRBlock(
+#                            geometry=resolve_geometry(block.geometry),
+#                            objectness_score=round(block.objectness_score, 2),
+#                            lines=[
+#                                OCRLine(
+#                                    geometry=resolve_geometry(line.geometry),
+#                                    objectness_score=round(line.objectness_score, 2),
+#                                    words=[
+#                                        OCRWord(
+#                                            value=word.value,
+#                                            geometry=resolve_geometry(word.geometry),
+#                                            objectness_score=round(word.objectness_score, 2),
+#                                            confidence=round(word.confidence, 2),
+#                                            crop_orientation=word.crop_orientation,
+#                                        )
+#                                        for word in line.words
+#                                    ],
+#                                )
+#                                for line in block.lines
+#                            ],
+#                        )
+#                        for block in page.blocks
+#                    ]
+#                )
+#            ],
+#        )
+#        for i, page in enumerate(out.pages)
+#    ]
+#
+#    return results
 
-    return results
+    # This returns a list of tuples (filename, page) that OCRmyPDF can use to apply hOCR to a PDF
+    # Each page contains a list of blocks, each block contains a list of lines
+    # Each line contains a list of words
+    # Each word contains a value, geometry, objectness_score, confidence, and crop_orientation
+    # Below we're exporting the results as hOCR, serializing it into binary bytes that the caller can
+    # consume to apply hOCR to a PDF
+    hOCR_list = out.export_as_xml()
+    hOCR_serial = pickle.dumps(hOCR_list)
+    # Empty the CUDA cache to free up memory
+    # This is important because the model is loaded in GPU memory and we need to free it up for other apps
+    torch.cuda.empty_cache()
+    return Response(content=hOCR_serial, media_type="application/octet-stream")
